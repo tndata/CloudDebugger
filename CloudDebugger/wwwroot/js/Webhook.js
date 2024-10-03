55cf0c63bdc25a7614a52bdf1cf814e38e85f188
Tore Nestenius <tore@tn-data.se>
2024-10-03 12:14:17 +0200

    Tweaks

--- CloudDebugger/wwwroot/js/Webhook.js
+++ CloudDebugger/wwwroot/js/Webhook.js
@@ -5,28 +5,28 @@
 var connection = new signalR.HubConnectionBuilder().withUrl("/WebHookHub").build();
 
 connection.on("ReceiveMessage1", function (color, content) {
-    let li = document.createElement("span");
-    document.getElementById("Messagebox1").appendChild(li);
-    li.style.color = color;
-    li.textContent = `${content} `;
+    let li1 = document.createElement("span");
+    document.getElementById("Messagebox1").appendChild(li1);
+    li1.style.color = color;
+    li1.textContent = `${content} `;
 });
 connection.on("ReceiveMessage2", function (color, content) {
-    let li = document.createElement("span");
-    document.getElementById("Messagebox2").appendChild(li);
-    li.style.color = color;
-    li.textContent = `${content} `;
+    let li2 = document.createElement("span");
+    document.getElementById("Messagebox2").appendChild(li2);
+    li2.style.color = color;
+    li2.textContent = `${content} `;
 });
 connection.on("ReceiveMessage3", function (color, content) {
-    let li = document.createElement("span");
-    document.getElementById("Messagebox3").appendChild(li);
-    li.style.color = color;
-    li.textContent = `${content} `;
+    let li3 = document.createElement("span");
+    document.getElementById("Messagebox3").appendChild(li3);
+    li3.style.color = color;
+    li3.textContent = `${content} `;
 });
 connection.on("ReceiveMessage4", function (color, content) {
-    let li = document.createElement("span");
-    document.getElementById("Messagebox4").appendChild(li);
-    li.style.color = color;
-    li.textContent = `${content}`;
+    let li4 = document.createElement("span");
+    document.getElementById("Messagebox4").appendChild(li4);
+    li4.style.color = color;
+    li4.textContent = `${content}`;
 });
 
 // Start the SignalR connection
