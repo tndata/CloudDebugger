500743cc977e9bf3f33a1f714739fe83ec6cde24
Tore Nestenius <tore@tn-data.se>
2024-10-03 12:06:54 +0200

    Improve JavaScript

--- CloudDebugger/wwwroot/js/Webhook.js
+++ CloudDebugger/wwwroot/js/Webhook.js
@@ -1,37 +1,37 @@
 ﻿"use strict";
 
+//SignalR connection script, used by the Webhook tool.
+
 var connection = new signalR.HubConnectionBuilder().withUrl("/WebHookHub").build();
 
 connection.on("ReceiveMessage1", function (color, content) {
-    var li = document.createElement("span");
+    let li = document.createElement("span");
     document.getElementById("Messagebox1").appendChild(li);
     li.style.color = color;
     li.textContent = `${content} `;
 });
 connection.on("ReceiveMessage2", function (color, content) {
-    var li = document.createElement("span");
+    let li = document.createElement("span");
     document.getElementById("Messagebox2").appendChild(li);
     li.style.color = color;
     li.textContent = `${content} `;
 });
 connection.on("ReceiveMessage3", function (color, content) {
-    var li = document.createElement("span");
+    let li = document.createElement("span");
     document.getElementById("Messagebox3").appendChild(li);
     li.style.color = color;
     li.textContent = `${content} `;
 });
 connection.on("ReceiveMessage4", function (color, content) {
-    var li = document.createElement("span");
+    let li = document.createElement("span");
     document.getElementById("Messagebox4").appendChild(li);
     li.style.color = color;
     li.textContent = `${content}`;
 });
 
-
-
+// Start the SignalR connection
 connection.start().then(function () {
-    //todo...
-
+    // Connection started
 }).catch(function (err) {
     return console.error(err.toString());
 });
