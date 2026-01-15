fe99bccf91bd1808c90fbf1a57f17da054dad191
Tore Nestenius <tore@tn-data.se>
2026-01-15 10:21:57 +0100

    Improved settings file.

--- AzureDeploymentScripts/_Settings.ps1
+++ AzureDeploymentScripts/_Settings.ps1
@@ -10,7 +10,7 @@
 # resource group name
 $rgname = 'rg-CloudDebugger'
 
-# location
+# Azure region - alternatives: 'northeurope', 'westeurope', 'eastus', 'westus2', 'germanywestcentral'
 $location = 'swedencentral'
 
 # User-assigned managed identity name
@@ -20,9 +20,10 @@ $identityName = 'mi-CloudDebugger'
 $AppServicePlan_linux = 'asp-CloudDebugger-Linux'
 $AppServicePlan_win = 'asp-CloudDebugger-Windows'
 
-# The SKU of the App Service Plans
-$AppServicePlanSKU_Linux = 'P0V3'     # S1 is not available in the region swedencentral?
-$AppServicePlanSKU_Windows = 'S1'     # Standard plan 
+# The SKU of the App Service Plans (B1, B2, B3, D1, F1, FREE, P0V3, P1MV3, S1, S2, S3, SHARED, ...)
+# Note: Not all SKUs are available in all regions
+$AppServicePlanSKU_Linux = 'P0V3'
+$AppServicePlanSKU_Windows = 'S1' 
 
 # The name of the App Services
 $AppServiceName_linux = 'as-CloudDebugger-Linux'
